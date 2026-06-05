122547784bf0e54fda5cd22669e1894353a9c652
Bernardo Vinhal <daciolohevertow@gmail.com>
2026-06-05 11:12:37 -0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -3,11 +3,13 @@ LARGURA_TELA = 800
 ALTURA_TELA = 600
 FPS = 60
 
-TITULO_JOGO = "Projeto Final - Pygame"
+TITULO_JOGO = "Jogo Pygame - Colete Gemas!"
 
 BRANCO = (255, 255, 255)
 PRETO = (0, 0, 0)
-CINZA = (212,212,212)
+CINZA = (200, 200, 200)
+VERDE = (0, 255, 0)
+VERMELHO = (255, 0, 0)
+AZUL = (0, 100, 255)
 
 CAMINHO_RECORDE = "data/recorde.txt"
-CAMINHO_SPRITES = "assets/imagens/spritesheet.bmp"
